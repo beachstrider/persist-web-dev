8324a2939f7fd706889850af63f3ace682ebbab4
Unique Solution <mernstackdev729@gmail.com>
2022-10-21 04:36:00 +0900

    remove: firebase api for security

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,15 +4,15 @@ import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import { connectFunctionsEmulator, getFunctions } from 'firebase/functions'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBY7zvf3f8h7JdH2-iWZ1_lZgNJeaKWPE",
-  authDomain: "persist-ai-client-portal.firebaseapp.com",
-  databaseURL: "https://persist-ai-client-portal-default-rtdb.firebaseio.com",
-  projectId: "persist-ai-client-portal",
-  storageBucket: "persist-ai-client-portal.appspot.com",
-  messagingSenderId: "283759597777",
-  appId: "1:283759597777:web:9d3dd64f031701f5b78df0",
-  measurementId: "G-8K6KL2YCQR"
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+}
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
